54dfc65d594854c0a4b0220fe155fb693054b6e5
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-06-13 20:57:05 -0300

    Salvando curso

---
+++ Módulo 1/_06_Casting.py
@@ -0,0 +1,23 @@
+#INT
+a = int(2) #Int
+b = int(2.8) #Float
+c = int('2') #String
+
+print (a,b,c)
+
+#FLOAT 
+w = float('2')
+x = float(2.3)
+y = float(2)
+z = float('2.3')
+
+print(w,x,y,z)
+
+#STRING
+t = str('s1')
+r = str(2)
+s = str(2.3)
+ 
+print("A variável é do tipo: ",type(t))
+print("A variável é do tipo: ",type(r))
+print("A variável é do tipo: ",type(s))
